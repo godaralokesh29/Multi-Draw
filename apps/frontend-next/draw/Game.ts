d7f0203a67132bb0498fe28eba9e06f7a45914d8
godaralokesh29 <godaralokesh2023@gmail.com>
2026-06-13 21:24:38 +0530

    Game.ts updated

--- apps/frontend-next/draw/Game.ts
+++ apps/frontend-next/draw/Game.ts
@@ -90,6 +90,14 @@ export class Game {
                 this.ctx.arc(shape.centerX, shape.centerY, Math.abs(shape.radius), 0, Math.PI * 2);
                 this.ctx.stroke();
                 this.ctx.closePath();                
+            } else if (shape.type === "pencil") {
+                this.ctx.strokeStyle = "rgba(255, 255, 255)";
+                this.ctx.lineWidth = 2;
+                this.ctx.beginPath();
+                this.ctx.moveTo(shape.startX, shape.startY);
+                this.ctx.lineTo(shape.endX, shape.endY);
+                this.ctx.stroke();
+                this.ctx.closePath();
             }
         })
     }
@@ -122,6 +130,14 @@ export class Game {
                 radius: radius,
                 centerX: this.startX + radius,
                 centerY: this.startY + radius,
+            }
+        } else if (selectedTool === "pencil") {
+            shape = {
+                type: "pencil",
+                startX: this.startX,
+                startY: this.startY,
+                endX: e.clientX,
+                endY: e.clientY
             }
         }
 
@@ -157,6 +173,13 @@ export class Game {
                 this.ctx.arc(centerX, centerY, Math.abs(radius), 0, Math.PI * 2);
                 this.ctx.stroke();
                 this.ctx.closePath();                
+            } else if (selectedTool === "pencil") {
+                this.ctx.lineWidth = 2;
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.startX, this.startY);
+                this.ctx.lineTo(e.clientX, e.clientY);
+                this.ctx.stroke();
+                this.ctx.closePath();
             }
         }
     }
